243f5c22819645457547bbfa657fc582e11855b1
REDYQ <137613963+REDYQ@users.noreply.github.com>
2026-05-14 23:57:38 +0300

    Update amusic.js

--- test/amusic/js/amusic.js
+++ test/amusic/js/amusic.js
@@ -14,7 +14,7 @@
             localStorage.setItem('rq_favorites', JSON.stringify(favorites));
         }
         document.getElementById('current-year').textContent = new Date().getFullYear();
-        const JSON_URL = 'https://raw.githubusercontent.com/REDYQ/Anime_Music/refs/heads/main/file/data.json';
+        const JSON_URL = 'https://raw.githubusercontent.com/REDYQ/Anime_Music/refs/heads/main/file/test/data.json';
         const BASE_URL = 'https://raw.githubusercontent.com/REDYQ/Anime_Music/refs/heads/main/file/';
         const frame = document.getElementById('player-frame');
         const mini = document.getElementById('mini-player');
